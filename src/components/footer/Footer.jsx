9cb8a5ba478664779761e9ba487a52c1f8d7c4bb
Anwar kamal <anwarkamal147@gmail.com>
2023-11-23 22:25:52 +0530

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                Immerse yourself in a world of endless entertainment with 
+                 this website, your one-stop destination for an unparalleled
+                experience. Discover a vast library of movies,
+                   TV shows all available at your fingertips. Whether you're craving 
+                   heart-pounding action, heartwarming comedies, or thought-provoking 
+                   dramas, has something for everyone. With new content added regularly,
+                     you'll never run out of fresh stories to explore.
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <FaFacebookF />
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
